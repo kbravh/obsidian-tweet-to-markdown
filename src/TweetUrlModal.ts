c717f5a09f3585a4ffbeb8e53da55f12a6606c81
Karey Higuera <karey.higuera@gmail.com>
2021-09-15 08:04:21 -0500

    add text field and button to modal

--- src/TweetUrlModal.ts
+++ src/TweetUrlModal.ts
@@ -1,14 +1,32 @@
-import { Modal, App } from "obsidian";
+import { App, Modal, Setting } from "obsidian";
 
 export class TweetUrlModal extends Modal {
-  constructor(app: App) {
+  constructor(app: App, private url: string) {
     super(app);
+    this.url = '';
   }
 
   onOpen() {
     let {contentEl, titleEl} = this;
     titleEl.setText('Download Tweet');
-    contentEl.setText('Modal!!');
+
+    new Setting(contentEl)
+      .setName('Tweet URL')
+      .setDesc('Enter the URL of the tweet to download.')
+      .addText(input => {
+        input.setValue(this.url)
+        input.onChange(value => this.url = value)
+        .setPlaceholder('Tweet URL')
+      })
+
+    new Setting(contentEl)
+      .setName('Download tweet')
+      .addButton(button => {
+        button.onClick(event => {
+          console.log(event, this.url)
+          this.close()
+        })
+      })
   }
 
   onClose() {
