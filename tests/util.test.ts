51c585401c104fb09d8ffa52cf0bad1ee27ede50
Karey Higuera <karey.higuera@gmail.com>
2022-04-11 22:08:22 -0500

    replace vitest with jest
    Vitest is blazing fast, but it's still immature and has terrible,
    unhelpful error messages. Jest is good enough.

--- tests/util.test.ts
+++ tests/util.test.ts
@@ -1,22 +1,19 @@
-import {assert, describe, it} from 'vitest'
-import {getTweetID} from 'src/util'
+import {getTweetID} from '../src/util'
 
-describe.concurrent('Tweet ID', async () => {
+describe('Tweet ID', () => {
   it('Extracts tweet Id from regular URL', async () => {
-    assert.equal(
-      getTweetID('https://twitter.com/whataweekhuh/status/1511656964538916868'),
-      '1511656964538916868'
-    )
+    expect(
+      getTweetID('https://twitter.com/whataweekhuh/status/1511656964538916868')
+    ).toBe('1511656964538916868')
   })
-  it('Extracts tweet Id from regular URL with query params', async () => {
-    assert.equal(
+  it('Extracts tweet Id from URL with query params', async () => {
+    expect(
       getTweetID(
         'https://twitter.com/whataweekhuh/status/1511656964538916868?s=20&t=tbYKVygf0nKOlvn4CpyKYw'
-      ),
-      '1511656964538916868'
-    )
+      )
+    ).toBe('1511656964538916868')
   })
   it('Errors on invalid URL', async () => {
-    assert.throws(() => getTweetID("I'm not a URL"))
+    expect(() => getTweetID('not-a-url')).toThrowError()
   })
 })
